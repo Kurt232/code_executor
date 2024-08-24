7fa55f8e1f6f393b216c29d64a3747e17d03d3fc
duwenjie <532923577@qq.com>
2024-08-24 18:16:07 +0800

    add: runner

--- executor.py
+++ executor.py
@@ -1,6 +1,6 @@
 from kernel import interface
 from kernel.ui_apis import CodeConfig, CodeStatus, regenerate_script, Verifier, ElementList
-
+from kernel.api_doc import ApiDoc
 class executor(object):
   '''
   '''
@@ -19,6 +19,30 @@ class executor(object):
     self.code_status.set_end_time()
 
     return
+
+if __name__ == '__main__':
+  save_path = None 
+  code = '''''' # todo::
+  doc = ApiDoc() # todo::
+  app_name = "" # ignore
+  env = interface.AsyncMockEnv([], []) # todo::
+  
+  compiled_code,  line_mappings= regenerate_script(code, 'verifier')
+  
+  code_config = CodeConfig(app_name, doc, save_path, code, compiled_code, line_mappings)
+  code_status = CodeStatus()
+  
+  runner = executor(env, code_config, code_status)
+  
+  done = False
+  try:
+    runner.run()
+    done = True
+  except Exception as e:
+    print(e)
+    pass
+  
+  print(f'{done=}')
 
 
 
